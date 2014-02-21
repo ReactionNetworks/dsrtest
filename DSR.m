51618ea15dc59158436019d8496f6beb46d39b49
Pete Donnell <pete.donnell@port.ac.uk>
2014-02-21 11:59:04 +0000

    Fix by Casian Pantea:   modified DSR.m to work with new cell/matrix syntax in Octave 3.6 Fix by Pete Donnell:   changed dsr from octave script to shell script   - auto-detects path to octave   - checks that the correct number of arguments were passed in

--- DSR.m
+++ DSR.m
@@ -1,5 +1,5 @@
 
-function [CYCLES, EVEN, ES, BADPAIRS, ADJ]=DSR(filename)
+function [CYCLES, EVEN, ES, BADPAIRS, ADJ]=debug(filename)
 %function [S_matrix, V_matrix]=readSVFile(filename)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -21,24 +21,24 @@ end
 
 line_data = fgetl(fid);
 
-S_matrix = [];
-V_matrix = [];
+S = [];
+V = [];
 
 line_string = sprintf(line_data, '%s'); % tries to parse string   
 split_data = strsplit(line_string,  " ");
 
-while (strcmp(line_string, 'V MATRIX') == 0)      
-   temp={};
-   size_temp=0;
+while (strcmp(line_string, 'V MATRIX') == 0)       
+temp=[];
 	
    for i=1:length(split_data)
 		if (strcmp(split_data(i),'') == 0)
-			size_temp = size_temp+1;
-			temp{size_temp} = deblank(split_data(i));
+			temp = [temp, str2num(deblank(split_data(i)){1,1})];
 		end			
-	end
-	S_matrix = [S_matrix; temp];
-	line_data = fgetl(fid);  
+   end
+     
+   S = [S; temp];
+
+   line_data = fgetl(fid);  
 
     line_string = sprintf(line_data, '%s'); % tries to parse string   
     split_data = strsplit(line_string, " ");
@@ -51,34 +51,20 @@ while (line_data ~= -1)   % makes sure its not eof
    split_data = strsplit(line_string, " ");
 
    
-   temp={};
-   size_temp=0;
-	
+      temp=[];
+      
    for i=1:length(split_data)
 		if (strcmp(split_data(i), '') == 0)
-			size_temp = size_temp+1;
-			temp{size_temp} = deblank(split_data(i));
+			temp = [temp,str2num(deblank(split_data(i)){1,1})];
 		end			
-	end
-	V_matrix = [V_matrix; temp];
+   end   
+	V = [V; temp];
+        
 	line_data = fgetl(fid); % reads one line of the file   
 end
 
 fclose(fid);
-
-[m,n]=size(S_matrix);
-
-S=zeros(m,n);
-V=zeros(m,n);
-
-for i=1:m
-  for j=1:n        
-    S(i,j)=str2num(S_matrix{i,j}{1,1});
-    V(i,j)=str2num(V_matrix{i,j}{1,1});
-  endfor
-endfor
-
-
+    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Analyze the DSR graph
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
